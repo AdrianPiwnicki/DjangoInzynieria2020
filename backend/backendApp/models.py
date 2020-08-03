74e0c1aa5bbb660a6f3361ead15acc2ed992c0ba
AdrianPiwnicki <piwnicki.adrian@gmail.com>
2020-08-03 20:48:20 +0200

    Gotowa baza danych

---
+++ backend/backendApp/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Produkty(models.Model):
+    class Przeliczniki(models.TextChoices):
+        kilogram = 'kg'
+        dekagram = 'dag'
+        gram = 'g'
+        sztuki = 'szt'
+        litr = 'l'
+        mililitr = 'ml'
+
+    class Kategorie(models.TextChoices):
+        Pozostale = 'pozostałe'
+        Owoce = 'owoce'
+        Warzywa = 'warzywa'
+        Zboza = 'zboża'
+        Nabial = 'nabiał'
+
+    nazwa = models.CharField(max_length=50, null=False)
+    przelicznik = models.CharField(choices=Przeliczniki.choices, default=Przeliczniki.kilogram, max_length=20)
+    grafika = models.FileField(blank=True, null=True)
+    kategoria = models.CharField(choices=Kategorie.choices, default=Kategorie.Pozostale, max_length=20)
+
+    def __str__(self):
+        return self.nazwa + " (" + str(self.kategoria) + ")"
+
+
+class Przepisy(models.Model):
+    nazwa = models.CharField(max_length=100, null=False)
+    przygotowanie = models.TextField()
+    czas = models.IntegerField()
+    photo = models.FileField(blank=True, null=True)
+    skladniki = models.ManyToManyField(Produkty)
+
+    def __str__(self):
+        return self.nazwa + " - " + str(self.czas) + " minut"
